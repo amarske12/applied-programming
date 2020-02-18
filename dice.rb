1f6b1f6d05419b4efcfb1c719f03641d0d5384bd
amarske12 <adammarske@gmail.com>
2020-02-18 02:23:35 +0000

    dice

--- dice.rb
+++ dice.rb
@@ -4,6 +4,14 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-  "<h1> Hello World </h1>"
+    @dice_one = rand(1..6)
+    @dice_two = rand(1..6)
+    @total = @dice_one + @dice_two
+    view "dice"
+
 end
 
+#"<h1> Game of Craps </h1>
+   # <p> Dice 1: #{dice_one} </p>
+    #<p> Dice 2: #{dice_two} </p>
+   # <p> The total: #{total} </p>"
